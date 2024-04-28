7e5291bd94f4d969d11cf42a782ee6d1d5babd71
BterceroIII <b1tercero.cruz@gmail.com>
2024-04-28 00:19:14 -0600

    Archivos para el inicio de conexion

---
+++ ProyectoData/EmpleadoData.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Options;
+using System.Data.SqlClient;
+using System.Data;
+using ProyectoModelo;
+
+namespace ProyectoData
+{
+    public class EmpleadoData
+    {
+        private readonly ConnectionsStrings conexiones;
+
+
+    }
+}
