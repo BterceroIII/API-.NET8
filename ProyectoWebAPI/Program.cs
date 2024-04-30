eec4b5db8c96945e6ce3a292770bfff9b4699301
BterceroIII <b1tercero.cruz@gmail.com>
2024-04-29 23:24:16 -0600

    Programs: Insertando servicio para conectar a la bases de datos appsettings.js: Insertando cadena de conexion

--- ProyectoWebAPI/Program.cs
+++ ProyectoWebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.DependencyInjection;
+using ProyectoData;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<ConnectionsStrings>(builder.Configuration.GetSection("ConnectionStrings"));
+builder.Services.AddSingleton<EmpleadoData>();
 
 var app = builder.Build();
 
